5d89a6a0a68fa6d1919f320e79296dffb14eceb6
Jose Cerqueira <jose@origensmedia.com>
2019-05-10 00:26:48 +0100

    Style added to the Layout component

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 export default ({ children, title }) => (
-	<div>
+	<div className="root">
 		<header>
 			<Link href="/">
 				<a>Home</a>
@@ -18,5 +18,50 @@ export default ({ children, title }) => (
 			{children}
 		</main>
 		<footer>&copy; {new Date().getFullYear()}</footer>
+		<style jsx>{`
+			.root {
+				display: flex;
+				justify-content: center;
+				align-items: center;
+				flex-direction: column;
+			}
+
+			main {
+				display: flex;
+				justify-content: center;
+				align-items: center;
+				flex-direction: column;
+			}
+
+			header {
+				width: 100%;
+				display: flex;
+				justify-content: space-around;
+				padding: 1em;
+				font-size: 1.2rem;
+				background: indigo;
+			}
+
+			header a {
+				color: darkgrey;
+				text-decoration: none;
+			}
+
+			header a:hover {
+				font-weight: bold;
+				color: lightgrey;
+			}
+
+			footer {
+				padding: 1em;
+			}
+		`}</style>
+		<style global jsx>{`
+			body {
+				margin: 0;
+				font-size: 110%;
+				background: #f0f0f0;
+			}
+		`}</style>
 	</div>
 );
