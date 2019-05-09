668d486597ae57dfe0aacb2c5aefb8a13db5872e
Jose Cerqueira <jose@origensmedia.com>
2019-05-10 00:35:37 +0100

    Set up Router and NProgress

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,26 @@
 import Link from "next/link";
+import Head from "next/head";
+import Router from "next/router";
+import NProgress from "nprogress";
+
+Router.onRouteChangeStart = url => {
+	console.log(url);
+	NProgress.start();
+};
+
+Router.onRouteChangeComplete = () => NProgress.done();
+
+Router.onRouteChaneErro = () => NProgress.done();
 
 export default ({ children, title }) => (
 	<div className="root">
+		<Head>
+			<title>NextPortfolio</title>
+			<link
+				rel="stylesheet"
+				href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
+			/>
+		</Head>
 		<header>
 			<Link href="/">
 				<a>Home</a>
